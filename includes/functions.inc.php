fd26e49002c9c77f685abb358f9c702e15957fc3
Mirek <gtamirec@gmail.com>
2021-05-05 23:40:30 +0200

    login
    login funckie

--- includes/functions.inc.php
+++ includes/functions.inc.php
@@ -54,7 +54,7 @@ function pwdMatch($pwd,$pwdRepeat) {
 
 }
 
-function uidExists($conn, $username) {
+function uidExists($conn, $username,$email) {
 $sql ="SELECT * FROM users WHERE usersUid=? OR usersEmail =? ;" ;
 $stmt = mysqli_stmt_init($conn);
 
@@ -103,5 +103,41 @@ mysqli_stmt_execute($stmt);
 mysqli_stmt_close($stmt);
 header('location: ../signup.php?error=none');
 exit();
+}
 
+
+function emptyInputLogin($username,$pwd) {
+
+  $result;
+
+  if (empty($username)|| empty($pwd)) {
+    $result =true;
+  }
+  else {
+    $result=false;
+  }
+  return $result;
+}
+
+function loginUser($conn,$username,$pwd){
+
+  $uidExists =uidExists($conn,$username,$username);
+  if ($uidExists==false) {
+    header('location: ../login.php?wronglogin');
+    exit();
+  }
+  $pwdHashed = $uidExists["usersPwd"];
+  $checkPwd=password_verify($pwd,$pwdHashed);
+
+  if ($checkPwd==false) {
+    header('location: ../login.php?wronglogin');
+    exit();
+  }
+  elseif ($checkPwd==true) {
+    session_start();
+    $_SESSION["userid"]= $uidExists["usersId"];
+    $_SESSION["useruid"]= $uidExists["usersUid"];
+    header('location: ../login.php');
+    exit();
+  }
 }
